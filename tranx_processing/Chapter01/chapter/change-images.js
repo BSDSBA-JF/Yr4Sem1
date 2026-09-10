61dc7f1bdad58225ca8d444fa7d4f1323e09efd1
BSDSBA-JF <JViray.BSDSBA2027@aim.edu>
2026-09-10 20:51:33 +0800

    added javascript source code to change the flower images when clicked

---
+++ tranx_processing/Chapter01/chapter/change-images.js
@@ -0,0 +1,26 @@
+// define variables containing plant file names
+let blanket = "blanket.jpg";
+let rugosa = "rugosa.jpg";
+let bluestem = "bluestem.jpg";
+
+// add event handlers to the list items
+document.getElementById("blanket").onclick = function() {
+    document.getElementById("plantImg").src = blanket;
+}
+document.getElementById("rugosa").onclick = function() {
+    document.getElementById("plantImg").src = rugosa;
+}
+document.getElementById("bluestem").onclick = function() {
+    document.getElementById("plantImg").src = bluestem;
+}
+
+/* 
+    Information on available plants 
+    incuding link to USDA website 
+*/
+let captionText = "<p> Plant choices for " + 
+"<a href='http://planthardiness.ars.usda.gov'>" + 
+"hardiness zones</a>" + 
+" 5a - 6b </p>"
+
+document.getElementById("imgCaption").innerHTML = captionText;
